75b90e8ba275728d5f363c1008cbce03fc183ad9
stu1 <>
2018-07-21 17:31:48 +0800

    test2

--- SwiftLecture02/ViewController.swift
+++ SwiftLecture02/ViewController.swift
@@ -13,8 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        print("Hello Swift!")
         
-        print("hello swift!")
     }
 
     override func didReceiveMemoryWarning() {
