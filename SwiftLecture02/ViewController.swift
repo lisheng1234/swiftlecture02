e1c53e69ec45d7057ae972cd33d4077f41f4670b
stu1 <>
2018-07-22 16:39:02 +0800

    no2

--- SwiftLecture02/ViewController.swift
+++ SwiftLecture02/ViewController.swift
@@ -23,19 +23,28 @@ class ViewController: UIViewController {
         var ints : [Int] = [2,1,3]
         var floats :Array<Float> = Array(repeating: 0, count: 5)
         ints.sort()
-        for index in 0..<3{
-            print("\(ints[index])")
-        }
-        
-        for str in english{
-            print(str)
-        }
+//        for index in 0..<3{
+//            print("\(ints[index])")
+//        }
+//
+//        for str in english{
+//            print(str)
+//        }
        //Dictionary
-        let 版本 = [1:"A",2:"C",3:"B"]
-        for(a,b) in 版本 {
+        let dic  = [1:"A",2:"C",3:"B"]
+        for(a,b) in dic {
             print("\(a):\(b)")
         }
+        let dicsorted = dic.sorted(by: {$0.value<$1.value})//以什么方式（key或value））排序
+        for(a,b) in dicsorted {
+            print("\(a):\(b)")
+        }
+        var times : Int = 0
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats:true, block: {smalltimer in
+            times = times + 1
+            print("timer:\(smalltimer.isValid)")})
         
+        _ = self.add(first: 1, second: 2)
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,6 +52,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func add(first:Int,second:Int) -> Int {
+        return first + second
+    }
 
+    
 }
 
